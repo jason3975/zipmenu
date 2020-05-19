2d672bd404aa9c71a14816b1bc645ef19f4bbd6b
Jason Jimenez <jasonjimenez3975@gmail.com>
2020-05-19 14:29:01 -0700

    Initial Commit

---
+++ Restaurant Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant Application
+//
+//  Created by Jason Jimenez on 5/19/20.
+//  Copyright © 2020 Jason Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
