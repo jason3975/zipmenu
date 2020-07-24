1a16748521fc503b5c8b9fabeccc4f0b01de9f0f
Alex Gaouette <alex.gaouette@gmail.com>
2020-07-23 17:52:32 -0700

    Reset Password Link working

--- Restaurant Application/View Controllers/ForgotPasswordViewController.swift
+++ Restaurant Application/View Controllers/ForgotPasswordViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 import FirebaseAuth
 
 class ForgotPasswordViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func closeView(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func sendResetPressed(_ sender: Any) {
+        if let email = emailTextfield.text {
+            Auth.auth().sendPasswordReset(withEmail: email) { error in
+                if let e = error {
+                    // TODO: Notify user of failed password reset
+                    print(e)
+                } else {
+                    // TODO: Notify user password email sent
+                    print("Password successfully reset")
+                }
+            }
+        }
+        
+
     }
     
 }
