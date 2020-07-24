e38ab10fa513da058a457e07920ea9d23d69be6d
Alex Gaouette <alex.gaouette@gmail.com>
2020-07-23 18:21:14 -0700

    Registration Implemented and Creates Firebase Account

--- Restaurant Application/View Controllers/SignInViewController.swift
+++ Restaurant Application/View Controllers/SignInViewController.swift
@@ -20,15 +20,20 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var signUpLine: UIView!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var loginLine: UIView!
-    @IBOutlet weak var emailTextfield: UITextField!
-    @IBOutlet weak var passwordTextfield: UITextField!
-    
     
     @IBOutlet weak var forgotPasswordButton: UIButton!
     //SIGNUP PAGE STACK VIEWS
     @IBOutlet weak var NameStack: UIStackView!
     @IBOutlet weak var LastNameStack: UIStackView!
     @IBOutlet weak var confirmStack: UIStackView!
+    
+    //SIGNIN/UP TEXTFIELDS
+    @IBOutlet weak var firstNameTextfield: UITextField!
+    @IBOutlet weak var lastNameTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var confirmPasswordTextfield: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +52,7 @@ class SignInViewController: UIViewController {
             loginButton.titleLabel?.font = UIFont(name: "Montserrat-Regular", size: 20)
             loginButton.setTitleColor(UIColor.black, for: .normal)
             loginLine.alpha = 0
-
+            
             UIView.animate(withDuration: 0.1, animations: {
                 //self.logoConstraint.constant = 40
                 //self.textEntryConstraint.constant = 40
@@ -57,7 +62,7 @@ class SignInViewController: UIViewController {
                 self.NameStack.isHidden = false
                 self.confirmStack.isHidden = false
                 self.signInButton.setTitle("Sign Up", for: .normal)
-                }, completion:  nil)
+            }, completion:  nil)
         }
         
     }
@@ -73,41 +78,67 @@ class SignInViewController: UIViewController {
             signUpLine.alpha = 0
             
             UIView.animate(withDuration: 0.1, animations: {
-            //self.logoConstraint.constant = 80
-            //self.navBarConstraint.constant = 60
-            //self.textEntryConstraint.constant = 60
-            self.forgotPasswordButton.isHidden = false
-            self.LastNameStack.isHidden = true
-            self.NameStack.isHidden = true
-            self.confirmStack.isHidden = true
-            self.signInButton.setTitle("Sign In", for: .normal)
-            
+                //self.logoConstraint.constant = 80
+                //self.navBarConstraint.constant = 60
+                //self.textEntryConstraint.constant = 60
+                self.forgotPasswordButton.isHidden = false
+                self.LastNameStack.isHidden = true
+                self.NameStack.isHidden = true
+                self.confirmStack.isHidden = true
+                self.signInButton.setTitle("Sign In", for: .normal)
+                
             }, completion:  nil)
         }
     }
     
     @IBAction func signInButtonPressed(_ sender: CustomButton) {
         
-        
-        if let email = emailTextfield.text, let password = passwordTextfield.text {
-            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-                if let e = error {
-                    // TODO: Notify users of incorrect credentials
-                    print(e)
-                } else {
-                    let storyboard = UIStoryboard(name: "LoginViews", bundle: nil)
-                    let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
-                    // This is to get the SceneDelegate object from your view controller
-                    // then call the change root view controller function to change to main tab bar
-                    (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+        switch signInButton.currentTitle {
+        case "Sign In":
+            if let email = emailTextfield.text, let password = passwordTextfield.text {
+                Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                    if let e = error {
+                        // TODO: Let user know there was a sign in error
+                        print(e)
+                    } else {
+                        self.navigateToMainView()
+                    }
                 }
             }
+        case "Sign Up":
+            if let email = emailTextfield.text,
+                let password = passwordTextfield.text,
+                let firstName = firstNameTextfield.text,
+                let lastName = lastNameTextfield.text,
+                let confirmPass = confirmPasswordTextfield.text {
+                if confirmPass == password {
+                    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                        if let e = error {
+                            // TODO: Notify user account creation failed
+                            print(e)
+                        } else {
+                            // Account creation succeeded
+                            // TODO: Implement user accounts stored using
+                            // a struct in Firestore to contain first/last/phone
+                            self.navigateToMainView()
+                            print("Account created successfully")
+                        }
+                    }
+                }
+            }
+        default:
+            print("This shouldn't have happened")
         }
+        
     }
     
     func navigateToMainView()
     {
-        
+        let storyboard = UIStoryboard(name: "LoginViews", bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+        // This is to get the SceneDelegate object from your view controller
+        // then call the change root view controller function to change to main tab bar
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
     }
 }
 
