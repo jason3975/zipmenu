79a37f9fc10e4a1e23c1ff0c039b6cce95cd4603
Jason Jimenez <jasonjimenez3975@gmail.com>
2020-07-24 23:05:39 -0700

    Refactoring of storyboards for tab bar controller, starting to work on home page layout

---
+++ Restaurant Application/View Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  ZipMenu
+//
+//  Created by Jason Jimenez on 7/24/20.
+//  Copyright © 2020 Jason Jimenez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class HomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        
+        
+    }
+}
